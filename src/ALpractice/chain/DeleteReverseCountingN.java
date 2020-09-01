d812678921fb66092a3a678a57522656c36d387b
asus <13101225226@163.com>
2020-09-01 22:14:11 +0800

    chain

--- src/ALpractice/chain/DeleteReverseCountingN.java
+++ src/ALpractice/chain/DeleteReverseCountingN.java
@@ -15,26 +15,64 @@ public class DeleteReverseCountingN {
      * @param n int整型
      * @return ListNode类
      */
+
     public ListNode removeNthFromEnd (ListNode head, int n) {
         int i =0;
         ListNode index1 = head;
         ListNode index2 = head;
-        if (head==null){
-            return head;
-        }
-        if (head.next==null){
+        int length = 1;
+        ListNode count = head;
+        if(head==null){
             return null;
         }
-        if (head.next.next==null){
-            if(n==1){
-                head.next =null;
-                return head;
-            }
-            else if(n==2){
-                head=head.next;
-                return head;
-            }
+        while(count.next!=null){
+            length++;
+            count = count.next;
         }
+        if(n>length){
+            return head;
+        }
+        if(length==1){
+            return null;
+        }
+        if (n==1) {
+            ListNode temp = head;
+            ListNode pre = null;
+            while (temp.next != null) {
+                pre = temp;
+                temp = temp.next;
+            }
+            pre.next = null;
+        }
+        if(n==length){
+            head = head.next;
+        }
+//        if (head==null){
+//            return head;
+//        }
+//        if (head.next==null){
+//            return null;
+//        }
+//        if (head.next.next==null){
+//            if(n==1){
+//                head.next =null;
+//                return head;
+//            }
+//            else if(n==2){
+//                head=head.next;
+//                return head;
+//            }
+//        }
+//        if (n==1) {
+//            ListNode temp = head;
+//            ListNode pre = null;
+//            while (temp.next != null) {
+//                pre = temp;
+//                temp = temp.next;
+//            }
+//            pre.next = null;
+//        }
+
         for (int j=1;j<n;j++){
             index1=index1.next;
         }
---
+++ src/ALpractice/chain/MergeTwoChain.java
@@ -0,0 +1,9 @@
+package ALpractice.chain;
+
+/**
+ * @author: XieY
+ * @date: 2020/9/1
+ */
+public class MergeTwoChain {
+
+}
